8593e94cb90e7742481af5aa7dac0d9e62154c60
Alex Sungurov <alexander.sungurov@inappstory.com>
2026-07-02 11:09:09 +0300

    chore: fixed back press handler

--- android/src/main/java/com/inappstorysdk/InAppStoryActivity.kt
+++ android/src/main/java/com/inappstorysdk/InAppStoryActivity.kt
@@ -7,50 +7,43 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import com.inappstorysdk.BackPressManager
-import android.util.Log
 
 open class InAppStoryActivity : ReactActivity() {
 
-    val backPressManager = BackPressManager() 
-    
-    /**
-     * Returns the name of the main component registered from JavaScript. This is used to schedule
-     * rendering of the component.
-     */
+    val backPressManager = BackPressManager()
+
+    private val backCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            if (backPressManager.shouldInterceptBackPress()) return  
+            isEnabled = false
+            this@InAppStoryActivity.onBackPressed()  
+            isEnabled = true
+        }
+    }
+
     override fun getMainComponentName(): String = "ias_activity"
 
-    /**
-     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-     */
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    val intercepted = backPressManager.shouldInterceptBackPress()
-                    if (!intercepted) {
-                        isEnabled = false
-                        onBackPressedDispatcher.onBackPressed()
-                        isEnabled = true
-                    }
-                }
-            })
+            onBackPressedDispatcher.addCallback(this, backCallback)
         }
+    }
+
+
+    override fun invokeDefaultOnBackPressed() {
+        backCallback.isEnabled = false
+        super.invokeDefaultOnBackPressed()
+        backCallback.isEnabled = true
     }
 
     @Suppress("DEPRECATION")
     override fun onBackPressed() {
-       if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
-            val intercepted = backPressManager.shouldInterceptBackPress()
-            if (intercepted) {
-                return
-            }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            if (backPressManager.shouldInterceptBackPress()) return
         }
         super.onBackPressed()
     }
